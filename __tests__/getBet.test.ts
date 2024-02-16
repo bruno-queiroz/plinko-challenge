71cda643c09227cf37e4e3e4f19cf3a8215ba23f
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-02-16 09:04:26 -0300

    Add linting

--- __tests__/getBet.test.ts
+++ __tests__/getBet.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import makeApp from "../src/app";
 
-const app = makeApp();
+const app = makeApp(() => console.log("mock database"));
 
 describe("Test /bet ending point", () => {
   it("GET to /bet without params should not be successful", async () => {
@@ -11,50 +11,50 @@ describe("Test /bet ending point", () => {
   });
 
   it("GET to /bet without risk param should fail", async () => {
-    const res = await request(app).get("/bet?bet=3&rows=8")
+    const res = await request(app).get("/bet?bet=3&rows=8");
 
-    expect(res.status).toBe(400)
-  })
+    expect(res.status).toBe(400);
+  });
 
   it("GET to /bet without rows param should fail", async () => {
-    const res = await request(app).get("/bet?bet=3&risk=high")
+    const res = await request(app).get("/bet?bet=3&risk=high");
 
-    expect(res.status).toBe(400)
-  })
+    expect(res.status).toBe(400);
+  });
 
   it("GET to /bet without bet param should fail", async () => {
-    const res = await request(app).get("/bet?rows=8&risk=high")
+    const res = await request(app).get("/bet?rows=8&risk=high");
 
-    expect(res.status).toBe(400)
-  })
+    expect(res.status).toBe(400);
+  });
 
   it("GET to /bet with all params should be successful", async () => {
-    const res = await request(app).get("/bet?bet=3&rows=8&risk=high")
+    const res = await request(app).get("/bet?bet=3&rows=8&risk=high");
 
-    expect(res.status).toBe(200)
-  })
+    expect(res.status).toBe(200);
+  });
 
   it("GET to /bet with less than 8 rows should fail", async () => {
-    const res = await request(app).get("/bet?bet=3&rows=7&risk=high")
+    const res = await request(app).get("/bet?bet=3&rows=7&risk=high");
 
-    expect(res.status).toBe(400)
-  })
+    expect(res.status).toBe(400);
+  });
 
   it("GET to /bet with more than 16 rows should fail", async () => {
-    const res = await request(app).get("/bet?bet=3&rows=17&risk=high")
+    const res = await request(app).get("/bet?bet=3&rows=17&risk=high");
 
-    expect(res.status).toBe(400)
-  })
+    expect(res.status).toBe(400);
+  });
 
   it("GET to /bet with bet amount as 0 should fail", async () => {
-    const res = await request(app).get("/bet?bet=0&rows=8&risk=high")
+    const res = await request(app).get("/bet?bet=0&rows=8&risk=high");
 
-    expect(res.status).toBe(400)
-  })
+    expect(res.status).toBe(400);
+  });
 
   it("GET to /bet with invalid risk option should fail", async () => {
-    const res = await request(app).get("/bet?bet=3&rows=8&risk=giant")
+    const res = await request(app).get("/bet?bet=3&rows=8&risk=giant");
 
-    expect(res.status).toBe(400)
-  })
+    expect(res.status).toBe(400);
+  });
 });
